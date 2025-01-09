d5e5f5ce80ce7f1b29bdb86458dd53c616289f20
NGC-1432 <131916088+NGC-1432@users.noreply.github.com>
2025-01-09 17:52:08 +0000

    dying is easy, living's harder

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,8 +1,20 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
 }
 
 function draw() {
-  background(200);
-  
+  background(115,28,122);
+  eye(150,300);
+  eye(450,300);
+}
+
+function eye(x,y){
+  fill("white")
+  ellipse(x,y,250,125)
+  fill("lavender")
+  ellipse(x,y,100,100)
+  fill("black")
+  ellipse(x,y,50,50)
+  fill("white")
+  ellipse(x-5,y-5,15,15)
 }
